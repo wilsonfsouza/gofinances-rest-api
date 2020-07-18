abef042727697976cc66e6afba06096cb2714e76
Wilson Franca <wilson.franca.92@gmail.com>
2020-07-18 13:54:15 -0700

    list, add, and delete transactions

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,19 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const transactionToBeDeleted = await transactionsRepository.findOne(id);
+
+    if (!transactionToBeDeleted) {
+      throw new AppError(
+        'The selected transaction could not be found or do not exist.',
+      );
+    }
+
+    await transactionsRepository.remove(transactionToBeDeleted);
   }
 }
 
