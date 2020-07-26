7006522e6e0be642cee6aa650fa8e32a4df73687
Wilson Franca <wilson.franca.92@gmail.com>
2020-07-25 20:33:54 -0700

    challenge 06 completed

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import { getCustomRepository } from 'typeorm';
+import multer from 'multer';
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
 import DeleteTransactionService from '../services/DeleteTransactionService';
-// import ImportTransactionsService from '../services/ImportTransactionsService';
+import uploadConfig from '../config/uploadFromCSV';
+import ImportTransactionsService from '../services/ImportTransactionsService';
 
+const uploadMiddleware = multer(uploadConfig);
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
@@ -42,8 +45,14 @@ transactionsRouter.delete('/:id', async (request, response) => {
   return response.status(204).send();
 });
 
-transactionsRouter.post('/import', async (request, response) => {
-  // TODO
-});
+transactionsRouter.post(
+  '/import',
+  uploadMiddleware.single('file'),
+  async (request, response) => {
+    const importTransactions = new ImportTransactionsService();
+    const transactions = await importTransactions.execute(request.file.path);
+    return response.json(transactions);
+  },
+);
 
 export default transactionsRouter;
--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,85 @@
+import { getCustomRepository, getRepository, In } from 'typeorm';
+import csvParse from 'csv-parse';
+import fs from 'fs';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface CSVTransaction {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute(filePath: string): Promise<Transaction[]> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoriesRepository = getRepository(Category);
+    const csvReadStream = fs.createReadStream(filePath);
+    const parser = csvParse({
+      from_line: 2,
+    });
+
+    const parseCSV = csvReadStream.pipe(parser);
+
+    const transactionsFromCSVList: CSVTransaction[] = [];
+    const categoriesFromCSVList: string[] = [];
+
+    parseCSV.on('data', async line => {
+      const [title, type, value, category] = line.map((cell: string) =>
+        cell.trim(),
+      );
+
+      if (!title || !type || !value) return;
+
+      categoriesFromCSVList.push(category);
+      transactionsFromCSVList.push({ title, type, value, category });
+    });
+    await new Promise(resolve => parseCSV.on('end', resolve));
+
+    const currentCategories = await categoriesRepository.find({
+      where: {
+        title: In(categoriesFromCSVList),
+      },
+    });
+
+    const currentCategoriesTitle = currentCategories.map(
+      (category: Category) => category.title,
+    );
+
+    const newCategories = categoriesFromCSVList
+      .filter(category => !currentCategoriesTitle.includes(category))
+      .filter((value, index, self) => self.indexOf(value) === index);
+
+    const newCategoriesToBeAdded = categoriesRepository.create(
+      newCategories.map(title => ({
+        title,
+      })),
+    );
+    await categoriesRepository.save(newCategoriesToBeAdded);
+
+    const allFinalCategories = [
+      ...newCategoriesToBeAdded,
+      ...currentCategories,
+    ];
+
+    const newTransactionsToBeAdded = transactionsRepository.create(
+      transactionsFromCSVList.map(transaction => ({
+        title: transaction.title,
+        type: transaction.type,
+        value: transaction.value,
+        category: allFinalCategories.find(
+          category => category.title === transaction.category,
+        ),
+      })),
+    );
+
+    await transactionsRepository.save(newTransactionsToBeAdded);
+
+    await fs.promises.unlink(filePath);
+
+    return newTransactionsToBeAdded;
   }
 }
 
